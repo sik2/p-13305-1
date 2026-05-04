f60989f6c69d5c0c4dd1555dd2e82e5982f16525
Unordinary <92520481+Unord1nary@users.noreply.github.com>
2026-05-04 16:53:18 +0900

    feat: 게시글 등록 기능 구현

--- src/main/java/com/back/back/article/ArticleController.java
+++ src/main/java/com/back/back/article/ArticleController.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +21,16 @@ public class ArticleController {
 	public String list(Model model) {
 		model.addAttribute("articles", articleService.findAll());
 		return "article/list";
+	}
+
+	@GetMapping("/article/create")
+	public String createForm() {
+		return "article/create";
+	}
+
+	@PostMapping("/article/create")
+	public String create(@RequestParam String title, @RequestParam String content) {
+		articleService.create(title, content);
+		return "redirect:/article/list";
 	}
 }
