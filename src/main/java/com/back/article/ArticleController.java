2d110a570a91728d8d9d49a08da469150ce755f1
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-05-04 17:42:06 +0900

    [feat] 1차 요구사항-1 완료

---
+++ src/main/java/com/back/article/ArticleController.java
@@ -0,0 +1,17 @@
+package com.back.article;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/article")
+public class ArticleController {
+
+    @GetMapping("/list")
+    public String list() {
+        return "article_list";
+    }
+}
