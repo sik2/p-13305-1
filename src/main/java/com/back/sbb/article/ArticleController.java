1519c4f5a811546b0a43a398ce5e6ec1e217892b
main <asd27090@gmail.com>
2026-05-04 17:31:39 +0900

    1차 테스트 커밋

--- src/main/java/com/back/sbb/article/ArticleController.java
+++ src/main/java/com/back/sbb/article/ArticleController.java
@@ -2,8 +2,11 @@ package com.back.sbb.article;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -12,7 +15,35 @@ public class ArticleController {
     private final ArticleService articleService;
 
     @GetMapping("/list")
-    public String list(){
+    public String list(Model model){
+
+        List<Article> articleList = articleService.findAll();
+
+        model.addAttribute("articleList",articleList);
+
         return "article_list";
+    }
+
+    @GetMapping("/create")
+    public String getAddContent(){
+
+        return "artice_create";
+    }
+
+    @PostMapping("/create")
+    public String postAddContent(@RequestParam(value = "title") String title,
+                                 @RequestParam(value = "content") String content){
+
+        articleService.create(title,content);
+        return "redirect:/article/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detailPage(@PathVariable("id") BigInteger id, Model model){
+
+        Article article = articleService.findById(id);
+        model.addAttribute("article",article);
+
+        return "article_detail";
     }
 }
--- src/main/java/com/back/sbb/article/ArticleService.java
+++ src/main/java/com/back/sbb/article/ArticleService.java
@@ -3,8 +3,33 @@ package com.back.sbb.article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class ArticleService {
     private final ArticleRespoitory articleRespoitory;
+
+    public List<Article> findAll() {
+        return articleRespoitory.findAll();
+    }
+
+    public void create(String title, String content)
+    {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+
+        articleRespoitory.save(article);
+    }
+
+    public Article findById(BigInteger id) {
+        Optional<Article> article = articleRespoitory.findById(id);
+
+        return article.get();
+    }
 }
