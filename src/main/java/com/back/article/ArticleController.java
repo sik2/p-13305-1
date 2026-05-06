189b8df7842e72376aeaedf400e4127e05926f33
hyunbeomlee1227-dev <hyunbeom.lee1227@gmail.com>
2026-05-06 16:52:07 +0900

    [feat] 1차요구사항-3 완료

--- src/main/java/com/back/article/ArticleController.java
+++ src/main/java/com/back/article/ArticleController.java
@@ -1,14 +1,17 @@
 package com.back.article;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
+    private final ArticleService articleService;
 
     @GetMapping("/list")
     public String list() {
@@ -17,6 +20,17 @@ public class ArticleController {
 
     @GetMapping("/create")
     public String create() {
+        return "article_create";
+    }
+
+    @PostMapping("/create")
+    public String createArticle(@RequestParam String title, @RequestParam String content) {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        articleService.save(article);
+
         return "article_create";
     }
 }
---
+++ src/main/java/com/back/article/ArticleService.java
@@ -0,0 +1,15 @@
+package com.back.article;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+
+    public Article save(Article article) {
+        return articleRepository.save(article);
+    }
+}
